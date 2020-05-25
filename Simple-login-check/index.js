cacdcdba43ed0e3f9171ffd45333e2f605e5f9b3
HongMuchan <muchang0824@gmail.com>
2020-05-25 10:04:37 +0900

    簡易ログインチェック

---
+++ Simple-login-check/index.js
@@ -0,0 +1,10 @@
+function test() {
+  num1 = prompt("合言葉");
+
+  if (num1 == "pass1") {
+    window.open("https://qiita.com/shuntaro_tamura/items/99adbe51132e0fb3c9e9");
+  } else {
+    alert("外れ");
+  }
+}
+test();
